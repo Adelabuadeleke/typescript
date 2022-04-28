d070775a3a153212728945ac0ddf3851dc2bcb4b
Adeleke <kamade123@gmail.com>
2022-04-28 22:26:40 +0100

    typescript basics

---
+++ typescript.js
@@ -0,0 +1,5 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var answer = sum(3, 5);
+console.log(answer);
---
+++ typescript.ts
@@ -0,0 +1,6 @@
+const sum = (a : number, b : number) => {
+  return a + b;
+}
+
+const answer = sum(3, 5);
+console.log(answer);
