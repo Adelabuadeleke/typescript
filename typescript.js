769889eca2dd21839d8b93777661b58e4ba372e7
Adeleke <kamade123@gmail.com>
2022-04-28 23:40:39 +0100

    typescript types explored

--- typescript.js
+++ typescript.js
@@ -1,5 +1,84 @@
-var sum = function (a, b) {
-    return a + b;
+// const sum = (a : number, b : number) => {
+//   return a + b;
+// }
+// const answer = sum(3, 5);
+// console.log(answer);
+// boolean
+var isCool = true;
+// number
+var age = 34;
+// string
+var eyeColor = 'blue';
+var favouriteQuote = "i'm not old im only ".concat(age);
+// Array
+var pets = [
+    'cat',
+    'dog',
+];
+var pets2 = [
+    'lion',
+    'dragon',
+    'zebra'
+];
+// Object
+var wizard = {
+    a: 'john'
 };
-var answer = sum(3, 5);
-console.log(answer);
+// null and undefined
+var meh = undefined;
+var noo = null;
+// Tuple
+var basket;
+basket = ['basket', 5];
+// Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size[2];
+var sizeNameNum = Size.Small;
+// Any - !!!!!!!! BE CAREFUL
+var whatever = true;
+// void
+var sing = function () {
+    console.log('when you see me come around...🎵');
+};
+// never
+// never for function does not return anything.
+var error = function () {
+    throw Error('oooop!');
+};
+var fightRobotArmy = function (robots) {
+    console.log('FIghT!');
+};
+var fightRobotArmyTwo = function (robots) {
+    console.log('FIghT!');
+};
+var dog = {};
+dog.count;
+// Function
+var fightRobotArmyTw = function (robots) {
+    console.log('FIghT!');
+};
+var fightRobotArmyThr = function (robots) {
+    console.log('FIghT!');
+    return 5;
+};
+// Classes
+var Animal = /** @class */ (function () {
+    function Animal(sound) {
+        this.sing = "lalalala";
+        this.sing = sound;
+    }
+    Animal.prototype.greet = function () {
+        return "Hello ".concat(this.sing);
+    };
+    return Animal;
+}());
+var lion = new Animal('RAAAAAWR');
+lion.sing;
+// Union
+var confused = true;
+var x = 4;
--- typescript.ts
+++ typescript.ts
@@ -1,6 +1,119 @@
-const sum = (a : number, b : number) => {
-  return a + b;
+// const sum = (a : number, b : number) => {
+//   return a + b;
+// }
+
+// const answer = sum(3, 5);
+// console.log(answer);
+
+// boolean
+let isCool: boolean = true
+
+// number
+let age: number = 34
+
+// string
+let eyeColor: string = 'blue'
+let favouriteQuote: string = `i'm not old im only ${age}`
+
+// Array
+let pets: string[] = [
+  'cat',
+  'dog',
+]
+
+let pets2: Array<string> = [
+  'lion',
+  'dragon',
+  'zebra'
+]
+
+// Object
+let wizard: object = {
+  a: 'john'
 }
 
-const answer = sum(3, 5);
-console.log(answer);
+// null and undefined
+
+let meh: undefined = undefined
+let noo: null = null
+
+// Tuple
+let basket:[string, number];
+basket = ['basket', 5]
+
+// Enum
+enum Size {Small = 1, Medium = 2, Large = 3}
+let sizeName: string = Size[2];
+let sizeNameNum: number = Size.Small;
+
+// Any - !!!!!!!! BE CAREFUL
+let whatever: any = true;
+
+// void
+let sing = ()=>{
+  console.log('when you see me come around...🎵')
+}
+
+// never
+// never for function does not return anything.
+let error = ():never => {
+  throw Error('oooop!')
+}
+
+// Interface
+interface RobotArmy {
+  count: number,
+  type: string,
+  magic: string
+}
+
+let fightRobotArmy = (robots: RobotArmy) => {
+  console.log('FIghT!')
+}
+
+let fightRobotArmyTwo = (robots: {count: number, type: string, magic: string}) => {
+  console.log('FIghT!')
+}
+
+// Type Assertion
+interface CatArmy {
+  count: number,
+  type: string,
+  magic?: string
+}
+
+let dog = {} as CatArmy
+dog.count
+
+// Function
+let fightRobotArmyTw = (robots: RobotArmy):void => {
+  console.log('FIghT!')
+}
+
+let fightRobotArmyThr = (robots: {count: number, type: string, magic: string}) => {
+  console.log('FIghT!')
+  return 5;
+}
+
+
+// Classes
+
+class Animal {
+  sing: string = `lalalala`
+  constructor(sound: string) {
+    this.sing = sound
+  }
+
+  greet() {
+    return `Hello ${this.sing}`
+  }
+}
+
+let lion = new Animal('RAAAAAWR')
+
+lion.sing
+
+// Union
+let confused: string | number | boolean = true
+
+let x = 4;
